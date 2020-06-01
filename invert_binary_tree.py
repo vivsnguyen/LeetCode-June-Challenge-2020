97c6f274bde44bbd00b7863c9abc11aadb16c328
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-06-01 11:55:38 -0700

    submission invert_binary_tree.py

--- invert_binary_tree.py
+++ invert_binary_tree.py
@@ -17,8 +17,33 @@ Output:
   7     2
  / \   / \
 9   6 3   1
+
 Trivia:
 This problem was inspired by this original tweet by Max Howell:
 
 Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so f*** off.
 """
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        
+        stack = [root]
+        
+        while stack:
+            node = stack.pop()
+            node.left, node.right = node.right, node.left 
+            
+            if node.right:
+                stack.append(node.right)
+                
+            if node.left:
+                stack.append(node.left)
+        return root
