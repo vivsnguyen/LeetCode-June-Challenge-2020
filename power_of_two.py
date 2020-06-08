915fe006e102d32d6dc5d993a37a0217000421e5
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-06-08 15:34:43 -0700

    submission power_of_two.py

--- power_of_two.py
+++ power_of_two.py
@@ -16,3 +16,10 @@ Input: 218
 Output: false
 
 """
+#bitwise operations???
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        while (n > 0) and (n % 2) == 0:
+            n = n/2
+        return n == 1
