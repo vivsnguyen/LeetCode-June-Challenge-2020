e1c4eb82ab2e3955f603653dc54a44791bfb21d3
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-06-10 14:20:17 -0700

    binary search soubmission search_insert_position.py

--- search_insert_position.py
+++ search_insert_position.py
@@ -20,3 +20,23 @@ Example 4:
 Input: [1,3,5,6], 0
 Output: 0
 """
+
+#binary search O(log(n))
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums)-1
+
+        while low <= high:
+            mid =  (low + high) // 2
+
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return low
+    
+    
